40a0eee27ade9e32c0a481f94452beee933eecef
aaron <aaronwong668@gmail.com>
2022-05-26 16:26:48 +0800

    added env parameters for URL

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-mongoose.connect(
-    "mongodb+srv://admin:afp8vwtamk@clusterfree.a8sal.mongodb.net/stshop?retryWrites=true&w=majority"
-).then((result) => {
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URL).then((result) => {
     console.log("Database connection successful");
 }).catch((err) => {
     console.log("error has occurred for database connection: " + err);
 });;
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("backend server has started");
 });
