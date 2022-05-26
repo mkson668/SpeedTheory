a4f6c8cb3db2a83a132082794a8385a64873413a
aaron <aaronwong668@gmail.com>
2022-05-26 15:38:43 +0800

    added database connection

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+mongoose.connect(
+    "mongodb+srv://admin:afp8vwtamk@clusterfree.a8sal.mongodb.net/stshop?retryWrites=true&w=majority"
+).then((result) => {
+    console.log("Database connection successful");
+}).catch((err) => {
+    console.log("error has occurred for database connection: " + err);
+});;
 
-
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log("backend server has started");
 });
