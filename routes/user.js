7abb0fab889b2f2ebdd744faf2d038c82a8e736e
aaron <aaronwong668@gmail.com>
2022-05-31 15:24:29 +0800

    add read and update for users with correct authorization

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 // setup express router
 const express = require("express");
 const router = express.Router();
-const {verifyToken, verifyTokenAndAuthorization} = require("./verifyToken");
+const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("./verifyToken");
 const CryptoJS = require("crypto-js");
 const User = require("../models/User");
 
@@ -18,6 +18,8 @@ router.post("/userposttest", (req, res) => {
 });
  */
 
+// CREATE 
+
 router.put("/:id", verifyTokenAndAuthorization, (req, res) => {
     // req.params.id is the supplied "/:id"
     if (req.body.password) {
@@ -29,10 +31,36 @@ router.put("/:id", verifyTokenAndAuthorization, (req, res) => {
         // kv in our database
         $set: req.body
     }).then((result) => {
-        // TODO: test basic behaviour of this promise in postman
+        // TODO: bug where promise result is not updated but database is updated
         res.status(200).json("update complete: " + result);
     }).catch((error) => {
         res.status(500).json("update failed: " + error);
+    })
+})
+
+// DELETE
+
+router.delete("/:id", verifyTokenAndAuthorization, (req, res) => {
+    User.findByIdAndDelete(req.params.id).then((result) => {
+        res.status(200).json("user successfully deleted: " + result);
+    }).catch((error) => {
+        res.status(500).json("user delete error: " + error);
+    })
+})
+
+// READ 
+
+router.get("/:id", verifyTokenAndAdmin, (req, res) => {
+    // only admin should be able to get all users
+    User.findById(req.params.id).then((result) => {
+
+        // should not expose password to browser
+        const {password, ...others} = result._doc;
+            
+        // destructure cause mongo is wrap it in _doc
+        res.status(200).json({...others});
+    }).catch((error) => {
+        res.status(500).json("user delete error: " + error);
     })
 })
 
