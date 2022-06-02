8ff4ff93c9cf92f1d9a7b8a58dd5824396fe2df6
aaron <aaronwong668@gmail.com>
2022-06-02 15:13:39 +0800

    added statistics api template will add more filters later

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("./verifyToken");
 const CryptoJS = require("crypto-js");
 const User = require("../models/User");
+const { route } = require("./authentication");
 
 /* 
 // for testing purposes
@@ -70,17 +71,73 @@ router.get("/find/:id", verifyTokenAndAdmin, (req, res) => {
 // for testing set one user to isAdmin:true in database manually
 router.get("/findAll", verifyTokenAndAdmin, (req, res) => {
     // only admin should be able to get all users
-    User.find().then((result) => {
-        const usersWOPassword = [];
-        for (const user of result) {
-            const {password, ...others} = user._doc;
-            usersWOPassword.push(others);
+    // add ?new=true parameter for retrieving only new users
+    const newUsersOnly = req.query.new;
+    if (newUsersOnly) {
+        User.find().then((result) => {
+            const usersWOPassword = [];
+            for (const user of result) {
+                const {password, ...others} = user._doc;
+                usersWOPassword.push(others);
+            }
+                
+            // destructure cause mongo is wrap it in _doc
+            //usersWOPassword.sort({username: -1}).limit(10);
+            usersWOPassword.sort((a, b) => b.username - a.username);
+            res.status(200).json(usersWOPassword.slice(0,5));
+        }).catch((error) => {
+            res.status(500).json("user delete error: " + error);
+        })
+    } else {
+        User.find().then((result) => {
+            const usersWOPassword = [];
+            for (const user of result) {
+                const {password, ...others} = user._doc;
+                usersWOPassword.push(others);
+            }
+                
+            // destructure cause mongo is wrap it in _doc
+            res.status(200).json(usersWOPassword);
+        }).catch((error) => {
+            res.status(500).json("user delete error: " + error);
+        })
+    }
+    
+})
+
+// GET user statistics
+router.get("/stats", verifyTokenAndAdmin, (req, res) => {
+    const date = new Date();
+    // set to year ago
+    const lastYear = new Date();
+    lastYear.setFullYear(lastYear.getFullYear() - 1, lastYear.getMonth());
+
+    User.aggregate([
+        {
+            $match: {
+                createdAt: {
+                    $gte: lastYear
+                }
+            }
+        },
+        {
+            $project: {
+                month: {
+                    $month: "$createdAt"
+                }
+            }
+        },
+        {
+            $group: {
+                _id: "$month",
+                total: {$sum: 1}
+            }
         }
-            
-        // destructure cause mongo is wrap it in _doc
-        res.status(200).json(usersWOPassword);
+    ]).then((data) => {
+        console.log("retrieving data from DB");
+        res.status(200).json(data);
     }).catch((error) => {
-        res.status(500).json("user delete error: " + error);
+        res.status(500).json("error with stats retrieval: " + error);
     })
 })
 
